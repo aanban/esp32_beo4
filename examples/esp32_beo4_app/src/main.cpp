0743e7e8250a69fff0c5d7c7589493564d7bd845
aanban <71218544+stillerGast@users.noreply.github.com>
2023-10-18 19:48:39 +0200

    beo_src_tbl() beo_cmd_tbl() from  IrBeoInfo.h

--- examples/esp32_beo4_app/src/main.cpp
+++ examples/esp32_beo4_app/src/main.cpp
@@ -1,8 +1,9 @@
 #include <Arduino.h>
 #include "IrBeo4.h"
+#include "IrBeo4Info.h"
 
-#define IR_RX_PIN 34            // IR receive pin
-IrBeo4 beo4(IR_RX_PIN);         // instance
+constexpr int8_t con_IR_RX_PIN = 34;  // IR receive pin
+IrBeo4 beo4(con_IR_RX_PIN);           // instance of beo4 decoder
 
 // solution with task and queue
 // 
@@ -18,19 +19,27 @@ void beo4_task(void *parameter) {
   }
 }
 
-
 // alternative solution with callback function
 //
 void beo_code_cb(uint32_t beo_code) {
   Serial.printf("beo_code_cb: %04x %s %s \n", beo_code,beo_src_tbl(beo_code),beo_cmd_tbl(beo_code));
 }
 
+// led call back function
+//
+constexpr uint8_t led_pin=5;
+void beo_led_cb(uint8_t mode){
+  digitalWrite(led_pin,mode);
+}
+
 void setup() {
   Serial.begin(115200);
+  pinMode(led_pin,OUTPUT);    // led for callback
+  digitalWrite(led_pin,HIGH); // led off
   beo4_rx_queue = xQueueCreate(50, sizeof(uint32_t));
   xTaskCreatePinnedToCore(beo4_task,"beo4_task",10000,NULL,0,&beo4_task_h,0);
   beo4.Begin(beo4_rx_queue);
-  printf("beo4 started, RX=%d\n",IR_RX_PIN);
+  printf("beo4 started, RX=%d\n",con_IR_RX_PIN);
 }
 
 void loop() {
