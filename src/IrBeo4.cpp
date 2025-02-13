371dd0ea5d2856ac83e48179f527bc90789a7ed2
aanban <stiller-gast@gmx.de>
2025-02-13 17:11:26 +0100

    handling repeatable beoCodes, IrBeo4Info.h removed

--- src/IrBeo4.cpp
+++ src/IrBeo4.cpp
@@ -1,7 +1,134 @@
 #include "IrBeo4.h"
 
+// ----------------------------------------------------------------------------
+// Helper c-functions 
+// ----------------------------------------------------------------------------
+
+// checks if a given beoCode inlcudes a beoCommand that is repeatable 
+// @param beoCode code to be checked
+// @return 0=standard 1=repeatable 
+uint8_t isRepeatable(uint32_t beo_code){
+  uint32_t beo_cmd = beo_code & 0xff; 
+  switch(beo_cmd){ 
+    case BEO_CMD_UP:
+    case BEO_CMD_DOWN: 
+    case BEO_CMD_LEFT: 
+    case BEO_CMD_RIGHT: 
+    case BEO_CMD_GO: 
+    case BEO_CMD_YELLOW: 
+    case BEO_CMD_GREEN: 
+    case BEO_CMD_BLUE: 
+    case BEO_CMD_RED:   return 1; 
+  }
+  return 0; 
+}
+
+
+// lookup the Beo4 source from a given beoCode
+// @param beo_code=Beo4 code
+// return Beo-Source or "invalid_src" for unknown beoCode
+const char* beo_src_tbl(uint32_t beo_code) {
+  uint8_t source=(uint8_t) ((beo_code>>8) & 255u);
+  switch(source) {
+    case BEO_SRC_VIDEO:  return(PSTR("video"));
+    case BEO_SRC_AUDIO:  return(PSTR("audio"));
+    case BEO_SRC_VTAPE:  return(PSTR("vtape"));
+    case BEO_SRC_ALL:    return(PSTR("all"));
+    case BEO_SRC_SPDEMO: return(PSTR("spdemo"));
+    case BEO_SRC_LIGHT:  return(PSTR("light"));
+  }
+  return(PSTR("invalid_src"));
+}
+
+// lookup the Beo4 command from a given beoCode
+// @param beo_code=Beo4 code 
+// return Beo-Source or "invalid_cmd" for unknwon beoCode
+const char* beo_cmd_tbl(uint32_t beo_code) {
+  uint8_t command=(uint8_t) (beo_code & 255u);
+  switch(command) {
+    case BEO_CMD_NUM_0:        return(PSTR("num-0"));
+    case BEO_CMD_NUM_1:        return(PSTR("num-1"));
+    case BEO_CMD_NUM_2:        return(PSTR("num-2"));
+    case BEO_CMD_NUM_3:        return(PSTR("num-3"));
+    case BEO_CMD_NUM_4:        return(PSTR("num-4"));
+    case BEO_CMD_NUM_5:        return(PSTR("num-5"));
+    case BEO_CMD_NUM_6:        return(PSTR("num-6"));
+    case BEO_CMD_NUM_7:        return(PSTR("num-7"));
+    case BEO_CMD_NUM_8:        return(PSTR("num-8"));
+    case BEO_CMD_NUM_9:        return(PSTR("num-9"));
+    case BEO_CMD_CLEAR:        return(PSTR("clear"));
+    case BEO_CMD_STORE:        return(PSTR("store"));
+    case BEO_CMD_STANDBY:      return(PSTR("standby"));
+    case BEO_CMD_MUTE:         return(PSTR("mute"));
+    case BEO_CMD_INDEX:        return(PSTR("index"));
+    case BEO_CMD_UP:           return(PSTR("up"));
+    case BEO_CMD_DOWN:         return(PSTR("down"));
+    case BEO_CMD_TUNE:         return(PSTR("tune"));
+    case BEO_CMD_CLOCK:        return(PSTR("clock"));
+    case BEO_CMD_FORMAT:       return(PSTR("format"));
+    case BEO_CMD_LEFT:         return(PSTR("left"));
+    case BEO_CMD_RETURN:       return(PSTR("return"));
+    case BEO_CMD_RIGHT:        return(PSTR("right"));
+    case BEO_CMD_GO:           return(PSTR("go"));
+    case BEO_CMD_STOP:         return(PSTR("stop"));
+    case BEO_CMD_RECORD:       return(PSTR("return"));
+    case BEO_CMD_SELECT:       return(PSTR("select"));
+    case BEO_CMD_SPEAKER:      return(PSTR("speaker"));
+    case BEO_CMD_PICTURE:      return(PSTR("picture"));
+    case BEO_CMD_TURN:         return(PSTR("turn"));
+    case BEO_CMD_LOUDNESS:     return(PSTR("loudness"));
+    case BEO_CMD_BASS:         return(PSTR("bass"));
+    case BEO_CMD_TREBLE:       return(PSTR("treble"));
+    case BEO_CMD_BALANCE:      return(PSTR("balance"));
+    case BEO_CMD_LIST:         return(PSTR("list"));
+    case BEO_CMD_MENU:         return(PSTR("menu"));
+    case BEO_CMD_VOL_UP:       return(PSTR("vol++"));
+    case BEO_CMD_VOL_DOWN:     return(PSTR("vol--"));
+    case BEO_CMD_LEFT_REPEAT:  return(PSTR("left++"));
+    case BEO_CMD_RIGHT_REPEAT: return(PSTR("right++"));
+    case BEO_CMD_UP_REPEAT:    return(PSTR("up++"));
+    case BEO_CMD_DOWN_REPEAT:  return(PSTR("down++"));
+    case BEO_CMD_GO_REPEAT:    return(PSTR("go++"));
+    case BEO_CMD_GREEN_REPEAT: return(PSTR("green++"));
+    case BEO_CMD_YELLOW_REPEAT:return(PSTR("yellow++"));
+    case BEO_CMD_BLUE_REPEAT:  return(PSTR("blue++"));
+    case BEO_CMD_RED_REPEAT:   return(PSTR("red++"));
+    case BEO_CMD_EXIT:         return(PSTR("exit"));
+    case BEO_CMD_TV:           return(PSTR("tv-on"));
+    case BEO_CMD_RADIO:        return(PSTR("radio-on"));
+    case BEO_CMD_VIDEO_AUX:    return(PSTR("video-aux"));
+    case BEO_CMD_AUDIO_AUX:    return(PSTR("audio-aux"));
+    case BEO_CMD_VTAPE:        return(PSTR("vtape-on"));
+    case BEO_CMD_DVD:          return(PSTR("dvd-on"));
+    case BEO_CMD_CAMCORD:      return(PSTR("camcord-on"));
+    case BEO_CMD_TEXT:         return(PSTR("text"));
+    case BEO_CMD_SP_DEMO:      return(PSTR("sp-demo"));
+    case BEO_CMD_SAT:          return(PSTR("sat-on"));
+    case BEO_CMD_PC:           return(PSTR("pc"));
+    case BEO_CMD_DOOR_CAM:     return(PSTR("door-cam"));
+    case BEO_CMD_ATAPE:        return(PSTR("atape-on"));
+    case BEO_CMD_CD:           return(PSTR("cd-on"));
+    case BEO_CMD_PHONO:        return(PSTR("phono-on"));
+    case BEO_CMD_ATAPE2:       return(PSTR("atape2-on"));
+    case BEO_CMD_CD2:          return(PSTR("cd2-on"));
+    case BEO_CMD_LIGHT:        return(PSTR("light-on"));
+    case BEO_CMD_AV:           return(PSTR("av"));
+    case BEO_CMD_YELLOW:       return(PSTR("yellow"));
+    case BEO_CMD_GREEN:        return(PSTR("green"));
+    case BEO_CMD_BLUE:         return(PSTR("blue"));
+    case BEO_CMD_RED:          return(PSTR("red"));
+  }
+  return(PSTR("invalid_cmd"));
+}
+
+
+// ----------------------------------------------------------------------------
 // global variables
+// ----------------------------------------------------------------------------
+
 DRAM_ATTR xQueueHandle       g_isr_queue;  // isr queue:  ir_pulse_isr() --queue--> beo4_rx_task() 
+DRAM_ATTR EventGroupHandle_t g_eg_handle;  // handle eventgroup repeated beocode event
+constexpr uint32_t evRptCode = (1 << 0);   // repeated-codes flag used by xEventGroupWaitBits()
 
 // Beo4 PulseCode = PulseWidth / 3125
 constexpr uint8_t bZero  = 1;  // pulsewidth= 3125
@@ -12,6 +139,35 @@ constexpr uint8_t bStart = 5;  // pulsewidth=15625
 constexpr uint8_t nBit   = 17; // data bits in frame
 
 static portMUX_TYPE beo4_tx_mutex = portMUX_INITIALIZER_UNLOCKED;
+
+
+// ----------------------------------------------------------------------------
+// Tasks
+// ----------------------------------------------------------------------------
+
+// handles repeatable BeoCodes that wait in quarantine
+void beo4_quarantine_task(void *handle){
+  if(NULL!=handle){
+    IrBeo4  *beo=(IrBeo4*) handle;  // get access to class
+    uint32_t repeatableBeoCode=0;   // repeatable BeoCode could have a successor
+    EventBits_t uxBits;             // used as trigger in case successor arrived
+    for(;;){
+      if(pdTRUE!=xQueueReceive(beo->m_beo4_quarantine_queue,&repeatableBeoCode,WAIT_infi)) {
+        ets_printf("xQueueReceive() something went wrong, waiting infinite does not work!");  
+      }
+      // repeatable BeoCode has arrived, wait for trigger-event or time out
+      uxBits=xEventGroupWaitBits(g_eg_handle, evRptCode, pdTRUE, pdTRUE, WAIT_250ms);
+      if(evRptCode==(uxBits & evRptCode)) { // successor BeoCode detected 
+        beo->m_beoWait=0;                   // reset flag, BeoCode is removed
+        //ets_printf(".\n");                        
+      } else {  // time out detected, delayed code can be send
+        xQueueSend(beo->m_beo4_rx_queue,&repeatableBeoCode,0);
+        beo->m_beoWait=0;                   // reset flag, BeoCode is send to standard qeuue
+        //ets_printf("_\n");
+      }
+    }
+  }
+}
 
 // receiver task
 void beo4_rx_task(void *handle) {
@@ -72,7 +228,9 @@ void beo4_tx_task(void *param) {
   } // if(NULL!=parameter)
 }
 
-// interrupt service routine
+// ----------------------------------------------------------------------------
+// Interrupt service routines
+// ----------------------------------------------------------------------------
 void IRAM_ATTR ir_pulse_isr(void) {
   static int64_t tsPre=0;              // timestamp of previous edge
   int64_t tsNew=esp_timer_get_time();  // timestamp of new edge
@@ -83,7 +241,7 @@ void IRAM_ATTR ir_pulse_isr(void) {
 }
 
 // ------------------------------------------------------------------------------------------------
-// member functions
+// Member functions
 // ------------------------------------------------------------------------------------------------
 
 // constructor
@@ -100,13 +258,19 @@ IrBeo4::~IrBeo4() {
 // initialize beo4 decoder
 int IrBeo4::Begin(QueueHandle_t beo4_rx_queue, QueueHandle_t beo4_tx_queue) {
   if(-1!=m_rx_pin && NULL!=beo4_rx_queue) { 
+    if(NULL==(m_beo4_quarantine_queue=xQueueCreate(5, sizeof(uint32_t))))
+      return -1;
+    if(pdPASS!=xTaskCreatePinnedToCore(beo4_quarantine_task,"beo4_quarantine_task",2048,this,1,&m_beo4_quarantine_task,1))
+      return -2; 
+    g_eg_handle = xEventGroupCreate(); // create event group for delayed beo codes
     m_beo4_rx_queue=beo4_rx_queue;
     if(NULL==(g_isr_queue=xQueueCreate(100, sizeof(int64_t))))
       return -1;
-    if(pdPASS!=xTaskCreatePinnedToCore(beo4_rx_task,"beo4_rx_task",2048,this,1,&m_beo4_rx_task,0))
+    if(pdPASS!=xTaskCreatePinnedToCore(beo4_rx_task,"beo4_rx_task",2048,this,1,&m_beo4_rx_task,1))
       return -2; 
     pinMode(m_rx_pin, INPUT_PULLUP);
     attachInterrupt(m_rx_pin, ir_pulse_isr, FALLING);
+
   } 
   if(-1!=m_tx_pin && NULL!=beo4_tx_queue) {
     m_beo4_tx_queue=beo4_tx_queue;
@@ -122,12 +286,12 @@ void IrBeo4::RxFsm(int64_t tsNew) {
   m_pWidth = (uint32_t)(m_tsNew-m_tsPre); // compute pulse-width from timestamps
   m_pCode  = (uint8_t)((m_pWidth+t_pc_half)/t_pc); // compute pulse-code from pulse-width (rounded)
   m_tsPre  = m_tsNew;          // set previous timestamp
-  switch(m_rxFSM) {            // process state machine
+  switch(m_rxFSM) {
     case rxSt::Idle: {         // wake up in idle state
       m_wait=WAIT_20ms;        // set short timeout within frame (supress TSOP7000 hiccups)
-      m_beoCode=0;             // new frame started -->  so reset beoCode and bit counter
-      m_bc=0;
-      m_tsFrm=m_tsNew;         // timestamp start-of-frame
+      m_beoCode=0;             // new frame started --> reset beoCode 
+      m_bc=0;                  // new frame started --> reset bit-counter
+      m_tsFrm=m_tsNew;         // take timestamp start-of-frame
       m_rxFSM=rxSt::S0;        // next state -->  begin sequence next
       break;
     } 
@@ -154,6 +318,13 @@ void IrBeo4::RxFsm(int64_t tsNew) {
     case rxSt::Start: {
       if(bStart==m_pCode) {
         m_rxFSM=rxSt::Data;    // next state -->  collect data
+        if(1==m_beoWait) {
+          m_beoWait=0;         
+          // a beoCode is waiting in quarantine and its successor beoCode 
+          // arrived, lets trigger the beo4_quarantine_task() to skip the 
+          // waiting BeoCode
+          xEventGroupSetBits(g_eg_handle,evRptCode); 
+        }
         LED(HIGH);             // active frame --> LED ON 
       } else {                 // frame corrupt
         resetRxFsm("ERR: Start state failed ");
@@ -173,15 +344,21 @@ void IrBeo4::RxFsm(int64_t tsNew) {
       }
       m_beoCode=(m_beoCode<<1)+curBit;  // shift Bit into beoCode
       if((++m_bc)==nBit) {
-        m_rxFSM=rxSt::Stop;     // next state -->  check the stopCode
+        m_rxFSM=rxSt::Stop;    // next state -->  check the stopCode
       }
       break;
     }
     case rxSt::Stop: {
       if(bStop==m_pCode) {     // final check of stop-code
         m_beoCode &= mskBC;    // mask and store valid beoCode
-        if(m_beo4_rx_queue) {  // send to queue
-          xQueueSend(m_beo4_rx_queue,&m_beoCode,0);
+        if(isRepeatable(m_beoCode)){ // is repeat-code possible
+          xQueueSend(m_beo4_quarantine_queue,&m_beoCode,0);
+          m_beoWait=1;         // send code into quarantine 
+        } else {
+          if(m_beo4_rx_queue){ // send to queue
+            m_beoWait=0;       // code not waiting 
+            xQueueSend(m_beo4_rx_queue,&m_beoCode,0);
+          }
         }
         resetRxFsm("SUCCESS beoCode OK ");
       } else {                 // frame corrupt 
--- src/IrBeo4.h
+++ src/IrBeo4.h
@@ -3,15 +3,124 @@
 
 #include "Arduino.h"
 
+
+// ----------------------------------------------------------------------------
+// Helper c-funtions const expressions
+// ----------------------------------------------------------------------------
+
+// beo4 source codes
+constexpr uint8_t BEO_SRC_VIDEO        = 0x00;
+constexpr uint8_t BEO_SRC_AUDIO        = 0x01;
+constexpr uint8_t BEO_SRC_VTAPE        = 0x05;
+constexpr uint8_t BEO_SRC_ALL          = 0x0F;
+constexpr uint8_t BEO_SRC_SPDEMO       = 0x1D;
+constexpr uint8_t BEO_SRC_LIGHT        = 0x1B;
+
+// beo4 command codes
+constexpr uint8_t BEO_CMD_NUM_0        = 0x00;
+constexpr uint8_t BEO_CMD_NUM_1        = 0x01;
+constexpr uint8_t BEO_CMD_NUM_2        = 0x02;
+constexpr uint8_t BEO_CMD_NUM_3        = 0x03;
+constexpr uint8_t BEO_CMD_NUM_4        = 0x04;
+constexpr uint8_t BEO_CMD_NUM_5        = 0x05;
+constexpr uint8_t BEO_CMD_NUM_6        = 0x06;
+constexpr uint8_t BEO_CMD_NUM_7        = 0x07;
+constexpr uint8_t BEO_CMD_NUM_8        = 0x08;
+constexpr uint8_t BEO_CMD_NUM_9        = 0x09;
+constexpr uint8_t BEO_CMD_CLEAR        = 0x0A;
+constexpr uint8_t BEO_CMD_STORE        = 0x0B;
+constexpr uint8_t BEO_CMD_STANDBY      = 0x0C;
+constexpr uint8_t BEO_CMD_MUTE         = 0x0D;
+constexpr uint8_t BEO_CMD_INDEX        = 0x0E;
+constexpr uint8_t BEO_CMD_UP           = 0x1E;
+constexpr uint8_t BEO_CMD_DOWN         = 0x1F;
+constexpr uint8_t BEO_CMD_TUNE         = 0x20;
+constexpr uint8_t BEO_CMD_CLOCK        = 0x28;
+constexpr uint8_t BEO_CMD_FORMAT       = 0x2A;
+constexpr uint8_t BEO_CMD_LEFT         = 0x32;
+constexpr uint8_t BEO_CMD_RETURN       = 0x33;
+constexpr uint8_t BEO_CMD_RIGHT        = 0x34;
+constexpr uint8_t BEO_CMD_GO           = 0x35;
+constexpr uint8_t BEO_CMD_STOP         = 0x36;
+constexpr uint8_t BEO_CMD_RECORD       = 0x37;
+constexpr uint8_t BEO_CMD_SELECT       = 0x3F;
+constexpr uint8_t BEO_CMD_SPEAKER      = 0x44;
+constexpr uint8_t BEO_CMD_PICTURE      = 0x45;
+constexpr uint8_t BEO_CMD_TURN         = 0x46; // list->mono
+constexpr uint8_t BEO_CMD_LOUDNESS     = 0x48;
+constexpr uint8_t BEO_CMD_BASS         = 0x4D;
+constexpr uint8_t BEO_CMD_TREBLE       = 0x4E;
+constexpr uint8_t BEO_CMD_BALANCE      = 0x4F;
+constexpr uint8_t BEO_CMD_LIST         = 0x58;
+constexpr uint8_t BEO_CMD_MENU         = 0x5C;
+constexpr uint8_t BEO_CMD_VOL_UP       = 0x60;
+constexpr uint8_t BEO_CMD_VOL_DOWN     = 0x64;
+constexpr uint8_t BEO_CMD_LEFT_REPEAT  = 0x70;
+constexpr uint8_t BEO_CMD_RIGHT_REPEAT = 0x71;
+constexpr uint8_t BEO_CMD_UP_REPEAT    = 0x72;
+constexpr uint8_t BEO_CMD_DOWN_REPEAT  = 0x73;
+constexpr uint8_t BEO_CMD_GO_REPEAT    = 0x75;
+constexpr uint8_t BEO_CMD_GREEN_REPEAT = 0x76;
+constexpr uint8_t BEO_CMD_YELLOW_REPEAT= 0x77;
+constexpr uint8_t BEO_CMD_BLUE_REPEAT  = 0x78;
+constexpr uint8_t BEO_CMD_RED_REPEAT   = 0x79;
+constexpr uint8_t BEO_CMD_EXIT         = 0x7F;
+constexpr uint8_t BEO_CMD_TV           = 0x80;
+constexpr uint8_t BEO_CMD_RADIO        = 0x81;
+constexpr uint8_t BEO_CMD_VIDEO_AUX    = 0x82;
+constexpr uint8_t BEO_CMD_AUDIO_AUX    = 0x83;
+constexpr uint8_t BEO_CMD_VTAPE        = 0x85;
+constexpr uint8_t BEO_CMD_DVD          = 0x86;
+constexpr uint8_t BEO_CMD_CAMCORD      = 0x87;
+constexpr uint8_t BEO_CMD_TEXT         = 0x88;
+constexpr uint8_t BEO_CMD_SP_DEMO      = 0x89;
+constexpr uint8_t BEO_CMD_SAT          = 0x8A;
+constexpr uint8_t BEO_CMD_PC           = 0x8B;
+constexpr uint8_t BEO_CMD_DOOR_CAM     = 0x8D;
+constexpr uint8_t BEO_CMD_ATAPE        = 0x91;
+constexpr uint8_t BEO_CMD_CD           = 0x92;
+constexpr uint8_t BEO_CMD_PHONO        = 0x93;
+constexpr uint8_t BEO_CMD_ATAPE2       = 0x94;
+constexpr uint8_t BEO_CMD_CD2          = 0x97;
+constexpr uint8_t BEO_CMD_LIGHT        = 0x9B;
+constexpr uint8_t BEO_CMD_AV           = 0xBF;
+constexpr uint8_t BEO_CMD_YELLOW       = 0xD4;
+constexpr uint8_t BEO_CMD_GREEN        = 0xD5;
+constexpr uint8_t BEO_CMD_BLUE         = 0xD8;
+constexpr uint8_t BEO_CMD_RED          = 0xD9;
+constexpr uint8_t BEO_CMD_STAND        = 0xf7; 
+
+extern const char* beo_src_tbl(uint32_t beo_code);
+extern const char* beo_cmd_tbl(uint32_t beo_code);
+extern uint8_t isRepeatable(uint32_t beo_code);
+
+// ----------------------------------------------------------------------------
 // external call back functions
+// ----------------------------------------------------------------------------
 extern __attribute__((weak)) void beo_led_cb(uint8_t mode);
+
+
+// ----------------------------------------------------------------------------
+// Interrupt service routines
+// ----------------------------------------------------------------------------
+
+// - triggers on rising edge of IR signal, 
+// - get timestamp and debounce possible noise pulses
+// - put valid timestamps to q_irs_queue 
+void IRAM_ATTR ir_pulse_isr(void);
+
+
+// ----------------------------------------------------------------------------
+// const expressions and enums
+// ----------------------------------------------------------------------------
 
 // Beo4 receicver state machine
 enum class rxSt { Idle, S0, S1, Start, Data, Stop };
 
 // timeout values, 20ms or infinite
-constexpr TickType_t WAIT_infi = portMAX_DELAY;  
-constexpr TickType_t WAIT_20ms = (20/portTICK_PERIOD_MS);
+constexpr TickType_t WAIT_infi  = portMAX_DELAY;  
+constexpr TickType_t WAIT_20ms  = (20/portTICK_PERIOD_MS);
+constexpr TickType_t WAIT_250ms = (250/portTICK_PERIOD_MS);
 
 // transmit timing
 constexpr uint64_t t_low_pulse=200;    ///< 455kHz carrier pulse time [microseconds]
@@ -24,16 +133,10 @@ constexpr uint32_t mskBC=0x0000FFFFul; ///< limit beoCode to 16 Bit
 // TSOP7000 noise debounce-time 
 constexpr int64_t t_debounce=600;      ///< debounce = 3 x t_low_pulse
 
-// interupt-serice-routine
-// - triggers on rising edge of IR signal, 
-// - get timestamp and debounce possible noise pulses
-// - put valid timestamps to q_irs_queue 
-void IRAM_ATTR ir_pulse_isr(void);
 
-// internal callback function for one-shot-timer
-// @param arg not used here
-void IRAM_ATTR one_shot_timer_cb(void* arg);
-
+// ----------------------------------------------------------------------------
+// the class
+// ----------------------------------------------------------------------------
 class IrBeo4 {
   private:
     int8_t     m_rx_pin  = -1;           ///< IR receive pin
@@ -46,11 +149,14 @@ class IrBeo4 {
     uint8_t    m_pCode   = 0;            ///< pulse code
     uint32_t   m_pWidth  = 0;            ///< pulse width 
     uint32_t   m_beoCode = 0;            ///< decoded Beo4 code
+    uint8_t    m_beoWait = 0;            ///< repeatable code is waiting 
     rxSt       m_rxFSM   = rxSt::Idle;   ///< initial state receive statemachine
     TickType_t m_wait    = WAIT_infi;    ///< initial timeout reading isr-queue
 
     QueueHandle_t m_beo4_rx_queue;       ///< output queue for received beoCodes
+    QueueHandle_t m_beo4_quarantine_queue;  ///< output queue for delayed received beoCodes
     QueueHandle_t m_beo4_tx_queue;       ///< input queue for beoCodes to be send
+    
     
     //  Beo4 receiver state machine
     //  gets timestamps from ISR-queue, computes PulseWith and converts to 
@@ -103,6 +209,18 @@ class IrBeo4 {
     // @param param handle to the class
     friend void beo4_rx_task(void *param); 
     TaskHandle_t m_beo4_rx_task; ///< task handle
+    
+    // Beo4 Quarantine Task, handels repeatable BeoCodes. 
+    // The beo_rx_task sends all repeatabel BeoCodes to the 
+    // m_beo4_quarantine_queue. They have to wait until either a timeout has 
+    // expired or a successor BeoCode arrived. In case of successor BeoCode 
+    // the event-Bis are set and the BeoCode will be skipped. In case of a
+    // timeout the BeoCode is forwarded to the m_beo4_rx_queue and is treated 
+    // as a single BeoCode.
+    // @param param the class handle
+    friend void beo4_quarantine_task(void *param); 
+    TaskHandle_t m_beo4_quarantine_task; ///< task handle
+    
     
     // Beo4 Transmit Task, reads beoCode from m_queue_beo_tx. The 455kHz carrier 
     // is generted by a PWM attached to m_ir_tx_pin ledcAttach(),ledcWrite(). The 
